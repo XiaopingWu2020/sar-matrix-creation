0184a3fc7c6b3ddcea64213e6110359b649dd045
Xiaoping Wu <65363290+XiaopingWu2020@users.noreply.github.com>
2024-05-16 23:40:48 -0500

    Update construct_Sv_voxsar.m

--- src/construct_Sv_voxsar.m
+++ src/construct_Sv_voxsar.m
@@ -145,17 +145,6 @@ disp('-> Voxel SAR Sv construction DONE...')
 %%
 %% ====================
 
-function outputMatrix = removeNaNs(inputMatrix)
-%***********************************************************************
-% function removes the NaN values from the input Matrix and sets to
-% zero values
-%
-%***********************************************************************
-bNaNMask = isnan(inputMatrix);
-
-outputMatrix = inputMatrix;
-outputMatrix(bNaNMask) = 0;
-
 %
 function kernel = designKernel()
 % construct the kernel for filtering the 3d sar distribution.
